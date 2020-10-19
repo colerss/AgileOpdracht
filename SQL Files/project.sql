6ce4bf3b77d88b13e94c1dcb9b920513405a5464
Bryant <bryantsuiskensmast@gmail.com>
2020-10-19 15:37:22 +0200

    Update project.sql

--- SQL Files/project.sql
+++ SQL Files/project.sql
@@ -1,41 +1,33 @@
+
+
 CREATE SCHEMA CWL
 GO
-
-CREATE TABLE CWL.LijstItem(
-LijstItemId int PRIMARY KEY,
-ProducttypeId int FOREIGN KEY REFERENCES CWL.Producttype(Id),
-WinkellijstId int FOREIGN KEY REFERENCES CWL.Winkellijst(Id),
-Aantal int NOT NULL
-);
-
-CREATE TABLE CWL.Winkellijst(
-WinkellijstId int PRIMARY KEY,
-GebruikerId int FOREIGN KEY REFERENCES CWL.Gebruiker(Id),
-Naam varchar(255)
-);
 
 CREATE TABLE CWL.Gebruiker(
 GebruikerId int PRIMARY KEY,
 Gebruikersnaam varchar (255),
 Wachtwoord varchar (255));
 
-CREATE TABLE CWL.Product(
-ProductId int PRIMARY KEY,
-ProducttypeId int FOREIGN KEY REFERENCES CWL.Producttype(Id),
-LocatieId int FOREIGN KEY REFERENCES CWL.Locatie(Id),
-Naam varchar (255),
-Prijs smallmoney,
-Hoeveelheid int,
-Omschrijving varchar (255)
+CREATE TABLE CWL.Locatie(
+LocatieId int PRIMARY KEY,
+Volgnummer int 
 );
-
-CREATE TABLE CWL.Producttype(
-ProducttypeId int PRIMARY KEY,
+CREATE TABLE CWL.Winkellijst(
+WinkellijstId int PRIMARY KEY,
+GebruikerId int FOREIGN KEY REFERENCES CWL.Gebruiker(GebruikerId),
 Naam varchar(255)
 );
 
-CREATE TABLE CWL.Locatie(
-LocatieId int PRIMARY KEY,
-ProducttypeId int FOREIGN KEY REFERENCES CWL.Producttype(Id),
-Volgnummer int 
+
+CREATE TABLE CWL.Product(
+ProductId int PRIMARY KEY,
+LocatieId int FOREIGN KEY REFERENCES CWL.Locatie(LocatieId),
+Naam varchar (255),
+Prijs smallmoney,
+Hoeveelheid int
+);
+CREATE TABLE CWL.LijstItem(
+LijstItemId int PRIMARY KEY,
+WinkellijstId int FOREIGN KEY REFERENCES CWL.Winkellijst(WinkellijstId),
+Aantal int NOT NULL
 );
