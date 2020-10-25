4e563190edc1f28a43ee11061499acc4ae5892d1
LaraPluys <61267694+LaraPluys@users.noreply.github.com>
2020-10-25 17:12:09 +0100

    artikellijst dynamisch

--- AgileWinkellijst/AgileWinkellijst/AssortimentWindow.xaml.cs
+++ AgileWinkellijst/AgileWinkellijst/AssortimentWindow.xaml.cs
@@ -38,5 +38,99 @@ namespace AgileWinkellijst
             Winkellijst.Show();
             this.Close();
         }
+
+        private Grid PopulatedGrid(SolidColorBrush color)
+        {
+            ColumnDefinition Col1 = new ColumnDefinition();
+            ColumnDefinition Col2 = new ColumnDefinition();
+            ColumnDefinition Col3 = new ColumnDefinition();
+            ColumnDefinition Col4 = new ColumnDefinition();
+            ColumnDefinition Col5 = new ColumnDefinition();
+
+            Col1.Width = new GridLength(8, GridUnitType.Star);
+            Col2.Width = new GridLength(2, GridUnitType.Star);
+            Col3.Width = new GridLength(2, GridUnitType.Star);
+            Col4.Width = new GridLength(1, GridUnitType.Star);
+            Col5.Width = new GridLength(1, GridUnitType.Star);
+
+            Grid sampleGrid = new Grid();
+
+            System.Windows.Shapes.Rectangle coloredRect = new System.Windows.Shapes.Rectangle();
+            Label lblProductnaam = new Label();
+            Label lblHoeveelheid = new Label();
+            Label lblPrijs = new Label();
+            Button btnEdit = new Button();
+            Button btnDelete = new Button();
+
+            sampleGrid.ColumnDefinitions.Add(Col1);
+            sampleGrid.ColumnDefinitions.Add(Col2);
+            sampleGrid.ColumnDefinitions.Add(Col3);
+            sampleGrid.ColumnDefinitions.Add(Col4);
+            sampleGrid.ColumnDefinitions.Add(Col5);
+
+            sampleGrid.RowDefinitions.Add(new RowDefinition());
+
+            Grid.SetRowSpan(coloredRect, 2);
+            Grid.SetColumnSpan(coloredRect, 5);
+            Grid.SetColumn(lblProductnaam, 0);
+            Grid.SetColumn(lblHoeveelheid, 1);
+            Grid.SetColumn(lblPrijs, 2);
+            Grid.SetColumn(btnEdit, 3);
+            Grid.SetColumn(btnDelete, 4);
+
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = Color.FromRgb(240, 248, 255);
+            coloredRect.Fill = mySolidColorBrush;
+
+            lblProductnaam.Content = "Calve Pindakaas 500g";
+            lblHoeveelheid.Content = "500g";
+            lblPrijs.Content = "€3.50";
+            btnEdit.Content = "+";
+            btnDelete.Content = "D";
+
+            sampleGrid.Children.Add(coloredRect);
+            sampleGrid.Children.Add(lblProductnaam);
+            sampleGrid.Children.Add(lblHoeveelheid);
+            sampleGrid.Children.Add(lblPrijs);
+            sampleGrid.Children.Add(btnDelete);
+            sampleGrid.Children.Add(btnEdit);
+
+            SolidColorBrush mySolidColorBrush2 = new SolidColorBrush();
+            mySolidColorBrush2.Color = Color.FromRgb(102, 130, 153);
+            btnDelete.Background = mySolidColorBrush2;
+            btnEdit.Background = mySolidColorBrush2;
+            btnDelete.BorderBrush = new SolidColorBrush(Colors.Black);
+            btnEdit.BorderBrush = new SolidColorBrush(Colors.Black);
+            btnEdit.Margin = new Thickness(0, 0, 0, 0);
+            btnDelete.Margin = new Thickness(0, 0, 0, 0);
+
+            
+           
+
+
+            return sampleGrid;
+        }
+
+        private Border NewBorder(SolidColorBrush color)
+        {
+            Border borderToAdd = new Border();
+            borderToAdd.Child = PopulatedGrid(color);
+            borderToAdd.BorderThickness = new Thickness(1);
+            borderToAdd.BorderBrush = new SolidColorBrush(Colors.Black);
+            return borderToAdd;
+        }
+
+        private void LoadElements()
+        {
+            spArtikellijst.Children.Clear();
+            spArtikellijst.Children.Add(NewBorder(new SolidColorBrush(System.Windows.Media.Color.FromRgb(200, 200, 200))));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadElements();
+        }
+
+       
     }
 }
