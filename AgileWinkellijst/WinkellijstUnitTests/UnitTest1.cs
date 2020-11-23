dce03aff5df850d35c08fd2a0ae9402a06a0817c
LaraPluys <61267694+LaraPluys@users.noreply.github.com>
2020-11-23 16:25:09 +0100

    unit test project

---
+++ AgileWinkellijst/WinkellijstUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinkellijstUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
