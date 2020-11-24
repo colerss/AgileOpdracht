948bc5810a214d2ca6e163e71f7b4e8b47db8813
StefanVDP <61277081+StefanVDP@users.noreply.github.com>
2020-11-24 18:18:43 +0100

    venster registreren

---
+++ AgileWinkellijst/AgileWinkellijst/RegistratieWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AgileWinkellijst
+{
+    /// <summary>
+    /// Interaction logic for RegistratieWindow.xaml
+    /// </summary>
+    public partial class RegistratieWindow : Window
+    {
+        public RegistratieWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistreren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
